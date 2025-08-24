c1a94129e40c4514a40b5931b155c51b0a701d1e
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-08-24 11:53:57 +0900

    VPCリソースを作ってみる

---
+++ aws/vpc/main.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "vpc" {
+    cidr_block = var.vpc_cidr_block
+
+    # mergeは後から定義されたキーのバリューで上書きされるため、必須であるものを後ろに定義する
+    tags = merge(
+        var.vpc_additional_tags,
+        {
+            Name = var.vpc_name
+            Env = var.env
+        }
+    )
+}
