8bdb9154f50220936419ce1efb0397a6a6d7de8d
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-08-26 23:31:57 +0900

    subnet_cidrsを定義

--- aws/vpc/resources.tf
+++ aws/vpc/resources.tf
@@ -1,8 +1,22 @@
+locals {
+    vpc_cidr = "10.0.0.0/16"
+}
+
 module "vpc" {
   source         = "./modules"
   service_name   = "terraform-vpc"
   env            = terraform.workspace
-  vpc_cidr_block = "10.0.0.0/16"
+  vpc_cidr_block = local.vpc_cidr
+  subnet_cidrs = {
+    public = [
+        cidrsubnet(local.vpc_cidr, 6, 1),
+        cidrsubnet(local.vpc_cidr, 6, 2),
+    ]
+    private = [
+        cidrsubnet(local.vpc_cidr, 6, 10),
+        cidrsubnet(local.vpc_cidr, 6, 20),
+    ]
+  }
   vpc_additional_tags = {
     Resource = "Terraform"
   }
