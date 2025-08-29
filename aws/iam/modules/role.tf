f9c1f4f2aa14eec5b834dc0396d405d55d87f334
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-08-29 18:04:37 +0900

    fix typo

--- aws/iam/modules/role.tf
+++ aws/iam/modules/role.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
         # sts.amazonaws.comに限定
         condition {
             test = "StringEquals"
-            variable = "token.actinos.githubusercontent.com:aud"
+            variable = "token.actions.githubusercontent.com:aud"
             values = [
                 "sts.amazonaws.com"
             ]
