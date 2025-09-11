210cc605c2f73449810ba66778c947981f8a2a0d
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-09-11 17:29:12 +0900

    自身のリソース参照の為書き換え

--- aws/ecs/modules/alb_dns_record.tf
+++ aws/ecs/modules/alb_dns_record.tf
@@ -3,8 +3,10 @@ resource "aws_route53_record" "record" {
     name = local.dns_a_record
     type = "A"
     alias {
-        name = module.alb.alb_fqdn
-        zone_id = module.alb.alb_zone_id
+        # aws_lb.alb_dns_nameとその下は同じterraform内のoutputの自己参照してる。
+        # これは循環じゃなくて依存関係だからOK
+        name = aws_lb.alb.dns_name
+        zone_id = aws_lb.alb.zone_id
         evaluate_target_health = false
     }
 }
