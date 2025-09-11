1c3b574fb2dd896ca19184a253bec501a80dc919
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-09-11 17:13:56 +0900

    terraform_remote_stateの為のoutputを作成

---
+++ aws/s3/output.tf
@@ -0,0 +1,11 @@
+output "alb_log_bucket_name" {
+    value = module.alb_log_bucket.alb_log_bucket_name
+}
+
+output "alb_log_bucket_arn" {
+    value = module.alb_log_bucket.alb_log_bucket_arn
+}
+
+output "alb_log_bucket_id" {
+    value = module.alb_log_bucket.alb_log_bucket_id
+}
